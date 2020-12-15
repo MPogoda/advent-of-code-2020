f1ab22d4753d9743f502afefb59508d0f739811d
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2020-12-15 13:03:05 +0200

    Day 15 use array

--- 15/task.js
+++ 15/task.js
@@ -1,18 +1,20 @@
+console.time("hm");
 function part1() {
     const input = [12,20,0,6,1,17,7];
     // const input = [0,3,6];
-    const map = new Map(input.map((v, i) => [v, i]));
+    const map = new Array(30000000);
+    input.forEach((v, i) => map[v] = i);
 
     let length = input.length;
 
     while (length < 30000000) {
         const prev = input[length - 1];
-        if (!map.has(prev)) {
+        if (map[prev] === undefined) {
             input.push(0);
         } else {
-            input.push(length - 1 - map.get(prev));
+            input.push(length - 1 - map[prev]);
         }
-        map.set(prev, length - 1);
+        map[prev] = length - 1;
         ++length;
     }
 
@@ -20,3 +22,4 @@ function part1() {
 }
 
 console.debug(part1());
+console.timeEnd("hm");
