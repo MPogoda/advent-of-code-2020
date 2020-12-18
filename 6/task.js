599864daaeaddf10e5d34177e413339a87905fde
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2020-12-18 08:09:48 +0200

    Added timer to day06.js

--- 6/task.js
+++ 6/task.js
@@ -1,16 +1,15 @@
-const fs = require('fs');
-
-const data = fs.readFileSync('input', 'UTF-8');
+const data = require('fs').readFileSync('input', 'UTF-8');
 const groups = data.split('\n\n');
 
+console.time("part2");
 const count = groups.reduce((acc, v) => {
     const lines = v.split('\n').filter((l) => l !== '');
     const c = Array.from('abcdefghijklmnopqrstuvwxyz')
         .filter((ch) => lines.every((line) => line.includes(ch)))
         .length;
-    console.debug({lines, c})
     return acc + c;
 }, 0);
+console.timeEnd("part2");
 
 console.log(count);
 
