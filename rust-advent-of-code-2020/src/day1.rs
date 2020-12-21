c76a063e12e2c394321002af811ec4d06f4e78f9
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2020-12-21 23:51:55 +0200

    Move day01.rs to aoc

---
+++ rust-advent-of-code-2020/src/day1.rs
@@ -0,0 +1,41 @@
+type SeenMap = [bool; 2020];
+type Input = (Vec<usize>, SeenMap);
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Input {
+    let mut result: Vec<_> = input
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .collect();
+
+    result.sort_unstable();
+
+    let mut seen = [false; 2020];
+    for &num in &result {
+        seen[num] = true;
+    }
+
+    (result, seen)
+}
+
+#[aoc(day1, part1)]
+pub fn part1((numbers, seen): &Input) -> usize {
+    for num in numbers {
+        let other = 2020 - num;
+        if seen[other] {
+            return other * num;
+        }
+    }
+    panic!("Couldn't find answer");
+}
+
+#[aoc(day1, part2)]
+pub fn part2((numbers, seen): &Input) -> usize {
+    for (i, a) in numbers.iter().enumerate() {
+        for b in numbers[(i+1)..].iter() {
+            if a + b >= 2020 { break }
+            let other = 2020 - a - b;
+            if seen[other] { return a * b * other; }
+        }
+    }
+    panic!("Couldn't find answer");
+}
