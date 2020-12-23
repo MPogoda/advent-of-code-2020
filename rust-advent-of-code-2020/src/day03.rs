94cda2c90437bbc23542b4edd90aa3daf22addfd
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2020-12-24 01:01:23 +0200

    Use &[u8] in day03.rs

--- rust-advent-of-code-2020/src/day03.rs
+++ rust-advent-of-code-2020/src/day03.rs
@@ -1,19 +1,19 @@
-fn check_slope(input: &str, x: usize, y: usize) -> usize {
+fn check_slope(input: &[u8], x: usize, y: usize) -> usize {
     input
-        .lines()
+        .split(|&ch| ch == b'\n')
         .step_by(y)
         .enumerate()
-        .filter(|(i, s)| s.chars().nth(x * i % s.len()) == Some('#'))
+        .filter(|(i, s)| s.get(x * i % s.len()) == Some(&b'#'))
         .count()
 }
 
 #[aoc(day3, part1)]
-pub fn part1(input: &str) -> usize {
+pub fn part1(input: &[u8]) -> usize {
     check_slope(input, 3, 1)
 }
 
 #[aoc(day3, part2)]
-pub fn part2(input: &str) -> usize {
+pub fn part2(input: &[u8]) -> usize {
     [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
         .iter()
         .fold(1, |acc, (x, y)| acc * check_slope(input, *x, *y))
