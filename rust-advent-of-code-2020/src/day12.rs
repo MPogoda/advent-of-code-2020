c1d8229a46510347af03cc240e950b93062d5f54
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2020-12-24 18:02:23 +0200

    Move day12.rs to aoc

--- rust-advent-of-code-2020/src/day12.rs
+++ rust-advent-of-code-2020/src/day12.rs
@@ -1,30 +1,12 @@
-use std::io::{self, BufRead};
-use std::time::{Instant};
+type Command = (u8, i64);
 
-fn main() {
-    let reading_timer = Instant::now();
-    let input = read_input();
-    println!("Read in {}μs", reading_timer.elapsed().as_micros());
-
-    let part1_timer = Instant::now();
-    let part1_result = part1(&input);
-    println!("Part 1 is {}, took {}μs", part1_result, part1_timer.elapsed().as_micros());
-
-    let part2_timer = Instant::now();
-    println!("Part 2 is {}, took {}μs", part2(&input, part1_result), part2_timer.elapsed().as_micros());
-}
-
-type Command = (char, i64);
-
-fn read_input() -> Vec<Command> {
-    io::stdin()
-        .lock()
+#[aoc_generator(day12)]
+pub fn read_input(input: &str) -> Vec<Command> {
+    input
         .lines()
-        .filter(|v| v.is_ok())
-        .map(|v| {
-            let s = v.unwrap();
-            let (cmd, val) = s.split_at(1);
-            (cmd.chars().nth(0).unwrap(), val.parse().unwrap())
+        .map(|line| {
+            let (cmd, val) = line.split_at(1);
+            (cmd.bytes().next().unwrap(), val.parse().unwrap())
         })
         .collect()
 }
@@ -34,42 +16,43 @@ fn move_coord((y, x): &Coord, (dy, dx): &Coord, n: i64) -> Coord {
     (y + n * dy, x + n * dx)
 }
 
-fn move_direction(coord: &Coord, direction: char, n: i64) -> Coord {
+fn move_direction(coord: &Coord, direction: u8, n: i64) -> Coord {
     move_coord(coord, match direction {
-        'N' => &(1, 0),
-        'E' => &(0, 1),
-        'S' => &(-1, 0),
-        'W' => &(0, -1),
+        b'N' => &(1, 0),
+        b'E' => &(0, 1),
+        b'S' => &(-1, 0),
+        b'W' => &(0, -1),
         _ => panic!("Wrong direction!")
     }, n)
 }
 
-const DIRECTIONS: [char; 4] = ['N', 'E', 'S', 'W'];
+const DIRECTIONS: &[u8; 4] = b"NESW";
 
 fn rotate_direction(dir: usize, n: i64) -> usize {
     let rotates = (360 + n) / 90;
     dir.wrapping_add(rotates as usize) % 4
 }
 
-fn part1(input: &[Command]) -> u64 {
+#[aoc(day12, part1)]
+fn part1(input: &[Command]) -> i64 {
     let mut dir = 1;
     // north, east
     let mut coords = (0, 0);
     for (cmd, num) in input {
         coords = match cmd {
-            'L' => {
+            b'L' => {
                 dir = rotate_direction(dir, -*num);
                 coords
             },
-            'R' => {
+            b'R' => {
                 dir = rotate_direction(dir, *num);
                 coords
             },
-            'F' => move_direction(&coords, DIRECTIONS[dir], *num),
+            b'F' => move_direction(&coords, DIRECTIONS[dir], *num),
             _ => move_direction(&coords, *cmd, *num)
         }
     }
-    (coords.0.wrapping_abs() + coords.1.wrapping_abs()) as u64
+    coords.0.wrapping_abs() + coords.1.wrapping_abs()
 }
 
 fn rotate_waypoint((y, x): &Coord, n: i64) -> Coord {
@@ -83,26 +66,27 @@ fn rotate_waypoint((y, x): &Coord, n: i64) -> Coord {
     }
 }
 
-fn part2(input: &[Command], _: u64) -> u64 {
+#[aoc(day12, part2)]
+fn part2(input: &[Command]) -> i64 {
     let mut waypoint = (1, 10);
     let mut coords = (0, 0);
 
     for (cmd, num) in input {
         coords = match cmd {
-            'L' => {
+            b'L' => {
                 waypoint = rotate_waypoint(&waypoint, -*num);
                 coords
             },
-            'R' => {
+            b'R' => {
                 waypoint = rotate_waypoint(&waypoint, *num);
                 coords
             },
-            'F' => move_coord(&coords, &waypoint, *num),
+            b'F' => move_coord(&coords, &waypoint, *num),
             _ => {
                 waypoint = move_direction(&waypoint, *cmd, *num);
                 coords
             }
         }
     }
-    (coords.0.wrapping_abs() + coords.1.wrapping_abs()) as u64
+    coords.0.wrapping_abs() + coords.1.wrapping_abs()
 }
